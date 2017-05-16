20e73ed64b29ba30fb3a438f2b0918ce1ec7dbb6
valentinatp <valentina.toledoporma@gmail.com>
2017-05-16 18:13:39 -0400

    Creo la ultima funcion de 'mouseover' y comento la funcion 'load' porque no corre el codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,19 @@ function principal(){
 	boton.addEventListener("click", function(){
 		alert("Hola mundox");
 	});
-	var load = document.getElementsByTagName("body");
+	/*var load = document.getElementsByTagName("body");
 	load.addEventListener("load", function(){
 		alert("Está cargada");
-	});
+	});*/
 	var sobre = document.getElementById("cajita");
 	sobre.addEventListener("mouseout", function(){
 		alert("No estás sobre mí");
 	});
-
+	var fuera = document.getElementById("caja");
+	fuera.addEventListener("mouseover", function(){
+		alert("Estás sobre mí");
+	});
 }
+
 principal();
 
